5f290601b3f2e30c782aa62ef4c2231d2d4bf20a
Eshun Joseph Rhule <ernesteshun456@gmail.com>
2024-08-05 16:00:21 +0000

    complete setup

---
+++ Sophron-by-Aphesis-Health/constants/features.tsx
@@ -0,0 +1,22 @@
+const basicFeatures = [
+    {
+    id:1,
+    name:'schedule appointment'
+},
+    {
+    id:2,
+    name:'talk to a doctor'
+},
+    {
+    id:3,
+    name:'see available doctors'
+},
+    {
+    id:4,
+    name:'history'
+},
+    {
+    id:5,
+    name:'settings'
+},
+]
